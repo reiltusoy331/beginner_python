a500984d484ba5a44d2accd8cb45634b40037896
reiltusoy331 <reiltusoy331@gmail.com>
2024-03-12 03:40:05 +0800

    Adding loopsQuiz.py to my github beginner_python repo.

---
+++ loopsQuiz.py
@@ -0,0 +1,8 @@
+count_number=0
+for number in range(1,10):                
+    if number % 2 == 0:        
+        print(number)
+        count_number+=1
+
+
+print(f"We have {count_number} even numbers.")
