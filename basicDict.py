58c93e0632b5bb49a18abb457fe7c319b2d313f4
reiltusoy331 <reiltusoy331@gmail.com>
2024-04-09 16:53:12 +0800

    Adding basicDict.py to my github beginner_python repo.

---
+++ basicDict.py
@@ -0,0 +1,12 @@
+point ={"x":1,"y":2}
+
+#adding key value pair to a dictionary
+point["x"]=10
+point["z"]=20
+
+print(point)
+
+#loop to a dictionary
+
+for key,value in point.items():
+    print(key,value)
