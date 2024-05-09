d4ddd940d65fed559f4674cceb618eec458db1e1
reiltusoy331 <reiltusoy331@gmail.com>
2024-05-09 20:00:32 +0800

    Adding swappingvalues.py to my github beginner_python repo.

---
+++ swappingvalues.py
@@ -0,0 +1,14 @@
+z=0
+x=10
+y=11
+
+print(f"Original values before swapping \n {x} \n {y}")
+
+z=x
+x=y
+y=z
+
+# other way to swap values
+# x,y = y,x
+
+print(f"New values after swapping \n {x} \n {y}")
